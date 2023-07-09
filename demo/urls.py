9a32eb9bb4dc8c552933acdac2193204255fd5c8
kevincw456 <119997828+kevincw456@users.noreply.github.com>
2023-07-10 00:14:58 +0800

    sss

--- demo/urls.py
+++ demo/urls.py
@@ -18,8 +18,14 @@ from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
 from account import views as a
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('account.urls')),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, documents_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, documents_root=settings.STATIC_ROOT)
