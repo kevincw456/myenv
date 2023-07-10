9c0e59fd616434f572ceda4f27191043e624513b
kevincw456 <119997828+kevincw456@users.noreply.github.com>
2023-07-10 11:49:30 +0800

    ssssss

--- account/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import tweets
-
-
-class tweetsSerializers(serializers.ModelSerializer):
-    class Meta:
-        model = tweets
-        fields='__all__'
